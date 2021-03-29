3657ebed798fafe1b9031a4e5ad0c2ef3469172c
Darko Vrbaski <vrbaski.ra200.2018@uns.ac.rs>
2021-03-29 20:58:41 +0200

    Implementirana registracija, izmena, brizanje i prikaz pacijanta u tabeli

--- PrviProgram/PrviProgram/Izgled/Sekretar/Registracija.xaml.cs
+++ PrviProgram/PrviProgram/Izgled/Sekretar/Registracija.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,24 +13,47 @@ using System.Windows.Shapes;
 
 namespace PrviProgram.Izgled.Sekretar
 {
-    /// <summary>
-    /// Interaction logic for Registracija.xaml
-    /// </summary>
     public partial class Registracija : Window
     {
-        public Registracija()
+        private Logika.LogikaSekretar.UpravljanjePacijentima up;
+        private ObservableCollection<Model.Pacijent> pacijenti;
+
+        public Registracija(ObservableCollection<Model.Pacijent> pacijenti)
         {
             InitializeComponent();
+            up = new Logika.LogikaSekretar.UpravljanjePacijentima();
+            this.pacijenti = pacijenti;
         }
 
         private void Potvrdi_Click(object sender, RoutedEventArgs e)
         {
+            Model.Korisnik korisnik = new Model.Korisnik();
+            korisnik.KorisnickoIme = textBoxKorisnickoIme.Text;
+            korisnik.Lozinka = textBoxLozinka.Password;
 
+            Model.Pacijent pacijent = new Model.Pacijent();
+            pacijent.Ime = textBoxIme.Text;
+            pacijent.Prezime = textBoxPrezime.Text;
+            pacijent.Jmbg = textBoxJMBG.Text;
+            pacijent.Email = textBoxEmail.Text;
+            pacijent.Pol = ((bool)radioButtonPolM.IsChecked ? Model.Pol.Muski : Model.Pol.Zenski);
+            pacijent.AdresaStanovanja = new Model.Adresa();
+            pacijent.MestoRodjenja = new Model.Grad();
+
+            pacijent.termin = new System.Collections.ArrayList();
+            pacijent.kartonPacijenta = new Model.KartonPacijenta();
+            pacijent.Korisnik = korisnik;
+
+            if (up.DodavanjePacijenta(pacijent) == true)
+            {
+                this.pacijenti.Add(pacijent);
+            }
+            this.Close();
         }
 
         private void Odustani_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
     }
 }
--- PrviProgram/PrviProgram/Izgled/Sekretar/WindowSekretar.xaml.cs
+++ PrviProgram/PrviProgram/Izgled/Sekretar/WindowSekretar.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,35 +10,39 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Logika.LogikaSekretar;
 
 namespace PrviProgram.Izgled.Sekretar
 {
     public partial class WindowSekretar : Window
     {
+        public UpravljanjePacijentima upravljanje;
+        public ObservableCollection<Model.Pacijent> pacijenti;
+
         public WindowSekretar()
         {
             InitializeComponent();
+            upravljanje = new UpravljanjePacijentima();
+            pacijenti = new ObservableCollection<Model.Pacijent>(upravljanje.PregledSvihPacijenata());
+            dgDataBinding.ItemsSource = pacijenti;
         }
 
         private void Izbrisi_Click(object sender, RoutedEventArgs e)
         {
-
+            upravljanje.BrisanjePacijenta((Model.Pacijent) dgDataBinding.SelectedItem);
+            pacijenti.Remove((Model.Pacijent)dgDataBinding.SelectedItem);
         }
 
         private void Izmeni_Click(object sender, RoutedEventArgs e)
         {
-
+            IzmenaPacijenta d = new IzmenaPacijenta(pacijenti, (Model.Pacijent)dgDataBinding.SelectedItem);
+            d.Show();
         }
 
         private void Dodaj_Click(object sender, RoutedEventArgs e)
         {
-            Registracija d = new Registracija();
+            Registracija d = new Registracija(pacijenti);
             d.Show();
-        }
-
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
