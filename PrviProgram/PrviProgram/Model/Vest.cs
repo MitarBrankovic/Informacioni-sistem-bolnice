79cf702d3ba56eb8568a9c58e5d32719986720d1
Darko Vrbaski <vrbaski.ra200.2018@uns.ac.rs>
2021-05-01 17:34:26 +0200

    #sekretar-crud-vesti implementirane metode repo, servisa i kotrolera za crud vesti

--- PrviProgram/PrviProgram/Model/Vest.cs
+++ PrviProgram/PrviProgram/Model/Vest.cs
@@ -8,7 +8,6 @@ namespace Model
         public string Vreme { get; set; }
         public string Tekst { get; set; }
         public string SifraVesti { get; set; }
-
         public Sekretar Autor { get; set; }
 
     }
