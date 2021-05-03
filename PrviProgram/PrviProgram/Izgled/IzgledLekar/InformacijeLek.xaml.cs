26ec3ba0301d78b4b2cd6fed52631dd432453956
SrdjanBeric <srdjan.beric123@gmail.com>
2021-05-03 17:48:06 +0100

    #pregled-lekova izmena-interfejsa

---
+++ PrviProgram/PrviProgram/Izgled/IzgledLekar/InformacijeLek.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrviProgram.Izgled.IzgledLekar
+{
+    /// <summary>
+    /// Interaction logic for InformacijeLek.xaml
+    /// </summary>
+    public partial class InformacijeLek : Window
+    {
+        public InformacijeLek(ObservableCollection<Lek> lekovi, Lek lek)
+        {
+            InitializeComponent();
+        }
+
+        private void Potvrdi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Odustani_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PrviProgram/PrviProgram/Izgled/IzgledLekar/WindowLekovi.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Model;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrviProgram.Izgled.IzgledLekar
+{
+    /// <summary>
+    /// Interaction logic for WindowLekovi.xaml
+    /// </summary>
+    public partial class WindowLekovi : Window
+    {
+        private ObservableCollection<Lek> lekovi;
+        private LekoviRepository lekoviRepository = new LekoviRepository();
+        public WindowLekovi()
+        {
+            InitializeComponent();
+            lekovi = new ObservableCollection<Lek>(lekoviRepository.PregledSvihLekova());
+            dataGridLekovi.ItemsSource = lekovi;
+        }
+
+        private void Komentarisi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Informacije_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
