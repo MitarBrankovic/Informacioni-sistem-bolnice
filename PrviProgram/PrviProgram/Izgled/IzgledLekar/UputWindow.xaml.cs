8f9efdc5dacb42ac778fb411454709b7bf48e2e4
SrdjanBeric <srdjan.beric123@gmail.com>
2021-05-04 16:55:29 +0200

    #uput-lekara prozor za uput

---
+++ PrviProgram/PrviProgram/Izgled/IzgledLekar/UputWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrviProgram.Izgled.IzgledLekar
+{
+    /// <summary>
+    /// Interaction logic for UputWindow.xaml
+    /// </summary>
+    public partial class UputWindow : Window
+    {
+        public UputWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Potvrdi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Otkazi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkBoxHitanSlucaj_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkBoxHitanSlucaj_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
