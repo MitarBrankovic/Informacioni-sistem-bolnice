19f6b008ac0bbd07015c2fb115e1d57f5f49daed
SrdjanBeric <srdjan.beric123@gmail.com>
2021-05-30 22:41:34 +0200

    #hci-help-wizard urađeni tooltipovi

--- PrviProgram/PrviProgram/Izgled/IzgledLekar/PocetniPrikaz.xaml.cs
+++ PrviProgram/PrviProgram/Izgled/IzgledLekar/PocetniPrikaz.xaml.cs
@@ -105,6 +105,27 @@ namespace PrviProgram.Izgled.IzgledLekar
             trenutniUserControl = noviUserControl;
         }
 
-        
+        private bool _isToolTipVisible = true;
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            Style style = new Style(typeof(ToolTip));
+            style.Setters.Add(new Setter(UIElement.VisibilityProperty, Visibility.Collapsed));
+            style.Seal();
+
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (_isToolTipVisible)
+                {
+                    window.Resources.Add(typeof(ToolTip), style); //hide
+                    _isToolTipVisible = false;
+                }
+                else
+                {
+                    window.Resources.Remove(typeof(ToolTip)); //show
+                    _isToolTipVisible = true;
+                }
+            }
+        }
+
     }
 }
